eaa86e34b67f0c02564d5e8f8394d665617f66c3
dbr <dbr.onix@gmail.com>
2012-01-22 23:26:14 +1030

    python -m framecurve somefile.framecurve.txt more useful

--- framecurve/__main__.py
+++ framecurve/__main__.py
@@ -1,6 +1,31 @@
+import optparse
+import fileinput
+
+import framecurve
+
+
+def main():
+    opter = optparse.OptionParser()
+    opts, args = opter.parse_args()
+
+    for fname in args:
+        f = open(fname)
+        v = framecurve.Validator(f)
+
+        if v.ok:
+            print "%s: ok" % fname
+
+        else:
+            print "%s: %d errors, %d warnings" % (
+                fname,
+                len(v.errors),
+                len(v.warnings))
+
+            for e in v.errors:
+                print "ERROR: %s" % e
+            for w in v.warnings:
+                print "WARNING: %s" % w
+
+
 if __name__ == '__main__':
-    import sys
-    import framecurve
-    fc = framecurve.Parser(
-        open(sys.argv[1])).parse()
-    print "\n".join([str(x) for x in fc])
+    main()
